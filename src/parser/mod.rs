2d44f2bf1883f137caf92fe3da262ef6f922095e
Matthew Slocum <arco000@gmail.com>
2016-06-24 13:59:58 -0700

    moved parsing rutine to parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -8,7 +8,64 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//#[macro_use]
+//extern crate rustc;
+//extern crate syntax;
+//extern crate rustc_plugin;
+
+use rustc_plugin::Registry;
+use syntax::ast::{MetaItem, Item, ItemKind, MetaItemKind, LitKind};
+use syntax::ext::base::{ExtCtxt, Annotatable};
+use syntax::ext::base::SyntaxExtension::MultiDecorator;
+use syntax::codemap::Span;
+use syntax::parse::token::intern;
+use syntax::ptr::P;
+
 // trash code [demo]
 pub fn demo() {
     println!("parser - reporting in");
+}
+
+
+
+// FIXME: this should be in the parser module!
+// Parse the condition arguments
+pub fn expand_args(args: &Vec<P<MetaItem>>) {
+    let mut builder = super::data::attr {pre: None, post: None};
+    match args.len() {
+        1 => {
+            println!("Found 1 argument:\n");
+            println!("{:?}\n", args[0]);
+            match args[0].node {
+                MetaItemKind::NameValue(ref x, ref y) =>
+                    {
+                        if x=="pre" {
+                            builder.pre = Some(y.node.clone());
+                        } else {
+                            builder.post = Some(y.node.clone());
+                        }
+                    },
+                _ => {},
+            }
+        },
+        2 => {
+            println!("Found 2 arguments:\n");
+            println!("{:?}\n", args[0]);
+            println!("{:?}\n", args[1]);
+            match args[0].node {
+                MetaItemKind::NameValue(ref x, ref y) => { builder.pre = Some(y.node.clone()); },
+                _ => {},
+            }
+            match args[1].node {
+                MetaItemKind::NameValue(ref x, ref y) => { builder.post = Some(y.node.clone()); },
+                _ => {},
+            }
+        },
+        _ => {
+            panic!("Too many arguments found for #[condition]; must have pre and/or post conditions");
+        }
+    }
+
+    println!("precondition {:?}", builder.pre.unwrap());
+    println!("postcondition {:?}", builder.post.unwrap());
 }
