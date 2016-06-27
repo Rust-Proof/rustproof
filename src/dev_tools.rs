2fb3d4dd58844ea066240881f418b326ccef6da0
Matthew Slocum <arco000@gmail.com>
2016-06-27 16:20:22 -0700

    added rust-doc comments to dev_tools function

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,5 +1,8 @@
+/// Prints the full type of argument provided
+/// # Arguments
+/// * *_* - any variable
 #[allow(unused_assignments)]
-pub fn print_type_of<T>(x: &T) -> () {
+pub fn print_type_of<T>(_: &T) -> () {
     let mut type_name = String::with_capacity(50);
     unsafe {
         type_name = (*super::std::intrinsics::type_name::<T>()).to_string();
