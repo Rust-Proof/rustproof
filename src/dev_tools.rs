58daad8021c25ac5e079b2e713cd6d777181075b
Matthew Slocum <arco000@gmail.com>
2016-06-27 16:22:47 -0700

    Revert "added rust-doc comments to dev_tools function"
    This reverts commit 2fb3d4dd58844ea066240881f418b326ccef6da0.

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -1,8 +1,5 @@
-/// Prints the full type of argument provided
-/// # Arguments
-/// * *_* - any variable
 #[allow(unused_assignments)]
-pub fn print_type_of<T>(_: &T) -> () {
+pub fn print_type_of<T>(x: &T) -> () {
     let mut type_name = String::with_capacity(50);
     unsafe {
         type_name = (*super::std::intrinsics::type_name::<T>()).to_string();
