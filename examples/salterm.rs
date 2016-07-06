4f36fb11edc61fcfcc640cc57d6b1641dcb45b11
Michael Salter <salterm@pdx.edu>
2016-07-06 13:04:13 -0700

    Changed Expression's print methods to include parentheses around everything, so the tree structure will be easier to visualize.

--- examples/salterm.rs
+++ examples/salterm.rs
@@ -4,10 +4,14 @@
 extern crate rustproof;
 
 use rustproof::expression::VariableMappingData;
+use rustproof::expression::Predicate;
+use rustproof::expression::AndData;
 
 fn main() {
 	let x = 7u32;
 	let z = foo(x);
+	let p = Predicate::And(AndData { p1: Box::new(Predicate::BooleanLiteral(true)), p2: Box::new(Predicate::BooleanLiteral(false)) });
+	println!("p: {}", p);
 }
 
 #[condition(pre="b", post="c")]
