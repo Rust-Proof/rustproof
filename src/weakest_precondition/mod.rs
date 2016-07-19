0bfd4037e3abf964a87a86ae8f82abb020049665
Sami Sahli <samisahli@me.com>
2016-07-18 18:11:07 -0700

    Fixed wp::gen(), fixed gen_stmt, fixed some comments

--- src/weakest_precondition/mod.rs
+++ src/weakest_precondition/mod.rs
@@ -21,39 +21,31 @@ use super::Attr;
 use super::expression;
 use expression::Predicate;
 use std::str::FromStr;
-use rustc::mir::repr::{Mir, BasicBlock, BasicBlockData, TerminatorKind};
+use rustc::mir::repr::{Mir, BasicBlock, BasicBlockData, TerminatorKind, StatementKind, Lvalue, Rvalue};
 use rustc_data_structures::indexed_vec::Idx;
 use super::parser;
 
 // computes the weakest precondition
-// FIXME: shouldnt return strings. Change to exression
-// FIXME: move to wp module
-// FIXME: do we want to return a predicate?
-pub fn wp(index: usize, data: &Vec<&BasicBlockData>, builder: &mut Attr) -> Option<Predicate> {
+pub fn gen(index: usize, data: &Vec<&BasicBlockData>, builder: &mut Attr) -> Option<Predicate> {
     println!("\n\nExamining bb{:?}\n{:#?}", index, data[index]);
 
-    // variables for tracking terminator
-    // targets of terminator (exits)
-    let mut block_targets = Vec::new();
-    // bb kind
-    let mut block_kind = "";
+    //let mut block_targets = Vec::new();
+    //let mut block_kind = "";
+    let mut wp: Option<Predicate> = None;
 
     // parse terminator data
     let terminator = data[index].terminator.clone().unwrap().kind;
     match terminator {
         TerminatorKind::Assert{cond, expected, msg, target, cleanup} => {
-            // FIXME: look into
-            //println!("{:#?}\n{:#?}\n{:#?}\n{:#?}\n{:#?}\n", cond, expected, msg, target, cleanup);
-            block_targets.push(target);
-            block_kind = "Assert";
+            wp = gen(target.index(), data, builder);
         },
         TerminatorKind::Return => {
             // return the post condition as expression to start WP gen
-            //return builder.post_expr.clone();
+            wp = builder.post_expr.clone();
+            println!("\n\n{:?}\n\n", wp);
         },
         TerminatorKind::Goto{target} => {
-            block_targets.push(target);
-            block_kind = "Goto";
+            wp = gen(target.index(), data, builder);
         },
         TerminatorKind::Call{func, args, destination, cleanup} => unimplemented!(),
         TerminatorKind::DropAndReplace{location, value, target, unwind} => unimplemented!(),
@@ -63,23 +55,9 @@ pub fn wp(index: usize, data: &Vec<&BasicBlockData>, builder: &mut Attr) -> Opti
         TerminatorKind::If{cond, targets} => unimplemented!(),
         TerminatorKind::Switch{discr, adt_def, targets} => unimplemented!(),
         TerminatorKind::SwitchInt{discr, switch_ty, values, targets} => unimplemented!(),
-        //_ => {}
     }
 
 
-    // recurse to exit points of CFG and save return values
-    let ret1: Option<Predicate> = None;
-    match block_kind {
-        "Assert" => {
-            let ret1 = wp(block_targets[0].index(), data, builder);
-        },
-        "Goto" => {
-            let ret1 = wp(block_targets[0].index(), data, builder);
-        },
-        _ => {
-            panic!("Unrecognized block kind");
-        }
-    }
 
     // FIXME: add wp generation
     // examine statements in reverse order
@@ -87,31 +65,45 @@ pub fn wp(index: usize, data: &Vec<&BasicBlockData>, builder: &mut Attr) -> Opti
     stmts.reverse();
     for stmt in stmts {
         //process stmt into expression
+        // stmt_substitution(wp, stmt);
         println!("{:?}", stmt);
     }
 
-    // FIXME: not this prob
-    return ret1;
+    println!("wp = {:?}", wp);
+
+    return wp;
 }
+
+
+
 
 
 //FIXME: wp is a predicate but is just a place holder for now. Will need appropriate type in
 //       function arguments
-pub fn stmt_substitution(wp: predicate, stmt: StatementKind)  {
-    let var_to_sub = stmt.Lvalue;
-    let sub_with = stmt.Rvalue;
-    match var_to_sub {
-        StatementKind::Lvalue::Var => {unimplemented!();}
-        StatementKind::Lvalue::Temp => {unimplemented!();}
-        StatementKind::Lvalue::Arg => {unimplemented!();}
-        StatementKind::Lvalue::Static => {unimplemented!();}
-        StatementKind::Lvalue::ReturnPointer => {unimplemented!();}
-        StatementKind::Lvalue::Projection => {unimplemented!();}
+pub fn stmt_substitution(wp: Predicate, stmt: StatementKind)  {
+
+    let mut lvalue: Option<Lvalue> = None;
+    let mut rvalue: Option<Rvalue> = None;
+    match stmt {
+        StatementKind::Assign(ref lval, ref rval) => {
+            lvalue = Some(lval.clone());
+            rvalue = Some(rval.clone());
+        }
     }
-    match sub_with {
-        StatementKind::Rvalue::CheckedBinaryOp => {unimplemented!();}
-        StatementKind::Rvalue::BinaryOp => {unimplemented!();}
-        StatementKind::Rvalue::UnaryOp => {unimplemented!();}
-        _=> {unimplemented!();}
+
+    match lvalue.unwrap() {
+        Lvalue::Var(ref var) => {unimplemented!();}
+        Lvalue::Temp(ref temp) => {unimplemented!();}
+        Lvalue::Arg(ref arg) => {unimplemented!();}
+        Lvalue::Static(ref def_id) => {unimplemented!();}
+        Lvalue::ReturnPointer => {unimplemented!();}
+        Lvalue::Projection(_) => {panic!("wtf is a projection");}
+    }
+
+    match rvalue.unwrap() {
+        Rvalue::CheckedBinaryOp(ref binop, ref lval, ref rval) => {unimplemented!();}
+        Rvalue::BinaryOp(ref binop, ref lval, ref rval) => {unimplemented!();}
+        Rvalue::UnaryOp(ref unop, ref val) => {unimplemented!();}
+        _=> {panic!("Only CheckedBinaryOp, BinaryOp, and UnaryOp currently supported");}
     }
 }
