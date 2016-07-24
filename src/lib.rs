9ac59bd59ab398a88aa5b11c0fe25d1b1056bfe9
Michael Salter <salterm@pdx.edu>
2016-07-23 21:12:20 -0700

    Cleaned up lib.rs. Fixed some comments, and added lines to ignore some common warnings (temporarily, of course).

--- src/lib.rs
+++ src/lib.rs
@@ -29,9 +29,11 @@
 // FIXME: useful for development, delete when project is "complete"
 #![allow(unused_variables)]
 #![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unused_assignments)]
 #![feature(core_intrinsics)]
 
-// extern crate imports
+// External crate imports
 #[macro_use]
 extern crate log;
 extern crate env_logger;
@@ -40,7 +42,7 @@ extern crate rustc_plugin;
 extern crate rustc_data_structures;
 extern crate syntax;
 
-// External use imports
+// External imports
 use env_logger::LogBuilder;
 use log::{LogRecord, LogLevelFilter};
 use rustc_data_structures::indexed_vec::Idx;
@@ -57,9 +59,8 @@ use syntax::ext::base::SyntaxExtension::MultiDecorator;
 use syntax::parse::token::intern;
 use syntax::ptr::P;
 
-// Local use imports
+// Local imports
 use expression::Predicate;
-
 
 // These are our modules
 pub mod expression;
@@ -67,9 +68,9 @@ pub mod parser;
 pub mod reporting;
 pub mod weakest_precondition;
 pub mod z3_interface;
-pub mod dev_tools; //FIXME: For debugging information, delete when project is "complete"
+pub mod dev_tools; // FIXME: For debugging information, delete when project is "complete"
 #[cfg(test)]
-mod tests; //Conditionally include tests when cargo --test is called
+mod tests; // Conditionally include tests when cargo --test is called
 
 #[derive(Debug)]
 pub struct Attr {
@@ -103,7 +104,7 @@ impl Attr {
 // Register plugin with compiler
 #[plugin_registrar]
 pub fn registrar(reg: &mut Registry) {
-	//This initializes the Reporting Module to Add the environment to the logger
+	// This initializes the Reporting Module to Add the environment to the logger
 	reporting::init();
 	//reg.register_syntax_extension(intern("condition"), MultiDecorator(Box::new(expand_condition)));
     //reg.register_syntax_extension(intern("condition"),
@@ -156,7 +157,6 @@ fn expand_bad_item(ctx: &mut ExtCtxt, span: Span) {
 }
 */
 
-
 struct MirVisitor {
     builder: Attr,
 }
@@ -167,9 +167,10 @@ impl <'tcx> Pass for MirVisitor {
 
 impl <'tcx> MirPass<'tcx> for MirVisitor {
     fn run_pass<'a>(&mut self, tcx: TyCtxt<'a, 'tcx, 'tcx>, src: MirSource, mir: &mut Mir<'tcx>) {
-        //clear the stored attributes in the builder
+        // Clear the stored attributes in the builder
         self.builder.clear();
 
+        // Store relevant data
         let item_id = src.item_id();
         let def_id = tcx.map.local_def_id(item_id);
         let name = tcx.item_path_str(def_id);
@@ -177,19 +178,20 @@ impl <'tcx> MirPass<'tcx> for MirVisitor {
 
         self.builder.func_name = name;
 
-
-
         // FIXME: I'm pretty sure this is a bad way to do this. but it does work.
         for attr in attrs {
             parser::parse_attribute(&mut self.builder, attr);
         }
 
-        // FIXME: better condition check
+        // FIXME: Better condition check
         if self.builder.pre_str != "" {
+            // Parse the pre- and postcondition arguments
             println!("{}", parser::parse_condition(self.builder.pre_str.as_str()));
             self.builder.pre_expr = Some(parser::parse_condition(self.builder.pre_str.as_str()));
             println!("{}", parser::parse_condition(self.builder.post_str.as_str()));
             self.builder.post_expr = Some(parser::parse_condition(self.builder.post_str.as_str()));
+
+            // Begin examining the MIR code
             MirVisitor::visit_mir(self, mir);
         }
     }
@@ -205,30 +207,30 @@ impl<'tcx> Visitor<'tcx> for MirVisitor {
         let mut temp_data = Vec::new();
         let mut var_data = Vec::new();
 
-        //grab basic block data from MIR
+        // Get the basic block data
         for index in 0..mir.basic_blocks().len() {
             let block = BasicBlock::new(index);
             block_data.push(&mir[block]);
         }
-        //grab arg_decls from MIR
+        // Get the function argument declarations
         for index in 0..mir.arg_decls.len() {
             let arg = Arg::new(index);
-            //arg_data is a vector of ArgDecl
+            // arg_data is a vector of ArgDecl
             arg_data.push(&mir.arg_decls[arg]);
         }
-        //grab temp_decls from MIR
+        // Get the temp declarations
         for index in 0..mir.temp_decls.len() {
             let temp = Temp::new(index);
-            //temp_data is a vector of TempDecl
+            // temp_data is a vector of TempDecl
             temp_data.push(&mir.temp_decls[temp]);
         }
-        //grab temp_decls from MIR
+        // Get the variable declarations
         for index in 0..mir.var_decls.len() {
             let var = Var::new(index);
-            //var_data is a vector of VarDecl
+            // var_data is a vector of VarDecl
             var_data.push(&mir.var_decls[var]);
         }
-        //now set them into the data tuple
+        // Store all the data
         let data = (arg_data, block_data, temp_data, var_data);
 
         // Generate the weakest precondition
