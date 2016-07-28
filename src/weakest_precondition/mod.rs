6b316ab117240c73b7bc779643c7059feeb019d8
Matthew Slocum <arco000@gmail.com>
2016-07-28 14:06:07 -0700

    added comment that is deleted line (incase we need it again)

--- src/weakest_precondition/mod.rs
+++ src/weakest_precondition/mod.rs
@@ -221,7 +221,7 @@ pub fn gen_stmt(mut wp: Predicate, stmt: Statement, data: &(Vec<&ArgDecl>, Vec<&
                     IntegerBinaryOperator::Subtraction
                 },
                 &BinOp::Mul => {
-                    
+
                     IntegerBinaryOperator::Multiplication
                 },
                 &BinOp::Shl => {
@@ -317,6 +317,7 @@ pub fn gen_lvalue(lvalue : Lvalue, data : &(Vec<&ArgDecl>, Vec<&BasicBlockData>,
         Lvalue::Var(ref var) => {
             // Find the name and type in the declaration
             VariableMappingData{ name: "var".to_string() + var.index().to_string().as_str(), var_type: data.3[var.index()].ty.clone().to_string() }
+            //VariableMappingData{ name: data.3[var.index()].name.to_string(), var_type: data.3[var.index()].ty.clone().to_string() }
         },
         // The returned value
         Lvalue::ReturnPointer => {
