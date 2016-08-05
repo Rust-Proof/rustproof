88e04414bb9be1c13f8ce21fda18eb3eeba3200e
Sami Sahli <samisahli@me.com>
2016-08-04 22:12:41 -0700

    Support for tuples

--- examples/ssahli.rs
+++ examples/ssahli.rs
@@ -13,25 +13,6 @@ fn main() {}
 // This is acceptable: it is placed over a function
 #[condition(pre="x: i32 > 0", post="return: i32 >= 5")]
 fn add_five_or_three(x: i32) -> i32 {
-    if x > 3 {
-        x + 5
-    }
-    else {
-        x + 3
-    }
+    let y = (5, x, 3, 7);
+    y.3
 }
-/*
-fn add_three(mut x: i32) -> i32 {
-    x = x + 3;
-    return x;
-}
-*/
-// This should error/warn: it isn't placed over a function
-/*
-#[condition(pre="x: int > 0", post="x: int >= 5")]
-struct Foo {
-    x: i32,
-}
-
-struct Bar;
-*/
