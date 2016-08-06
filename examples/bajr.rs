fa3d4a7dd1789261b29f0cc425eaa5172ce2369c
Bradley Rasmussen <rasmbj@pdx.edu>
2016-08-06 13:36:54 -0700

    Derp2

--- examples/bajr.rs
+++ examples/bajr.rs
@@ -1,3 +1,12 @@
-//extern crate rustproof;
+#![feature(plugin, custom_attribute)]
+#![plugin(rustproof)]
+#![allow(dead_code)]
 
-fn main() {}
+fn main() {
+    add_five(0);
+}
+
+#[condition(pre="true", post="true")]
+fn add_five(x: i32) {
+    assert!(1 < 2);
+}
