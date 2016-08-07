5ebc815e91d839ba8606ca5c7494d9db3d013b58
Bradley Rasmussen <rasmbj@pdx.edu>
2016-08-06 19:06:20 -0700

    Fixed indentation and formatting

--- examples/bajr.rs
+++ examples/bajr.rs
@@ -1,12 +1,14 @@
 #![feature(plugin, custom_attribute)]
 #![plugin(rustproof)]
 #![allow(dead_code)]
+
 fn main() {
     let x = add_five(5);
-    let y = add_two(5);
+//      let y = add_two(5);
 }
 
-#[condition(pre="x:i32 < 2147483642:i32 && x:i32 > -2147483648:i32", post="return:i32 == x:i32 + 5:i32")]
+#[condition(pre="x:i32 < 2147483642:i32 && x:i32 > -2147483648:i32",
+            post="return:i32 == x:i32 + 5:i32")]
 fn add_five(x: i32) -> i32 {
     if false {
         x+2
@@ -14,8 +16,9 @@ fn add_five(x: i32) -> i32 {
         x+2
     }
 }
-
-#[condition(pre="x:i32 < 2147483642:i32 && x:i32 > -2147483648:i32", post="return:i32 == (x:i32 + 5:i32)")]
-fn add_two(x: i32) -> i32 {
-    x+5
-}
+//  
+//  #[condition(pre="x:i32 < 2147483642:i32 && x:i32 > -2147483648:i32",
+//              post="return:i32 == (x:i32 + 5:i32)")]
+//  fn add_two(x: i32) -> i32 {
+//      x+5
+//  }
