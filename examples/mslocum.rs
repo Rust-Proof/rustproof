98be2ae8f343f39132726b71f38479bf4f326ccb
Matthew Slocum <arco000@gmail.com>
2016-08-07 01:03:25 -0700

    fixed my example

--- examples/mslocum.rs
+++ examples/mslocum.rs
@@ -2,19 +2,16 @@
 #![plugin(rustproof)]
 #![allow(dead_code)]
 
-fn main() {
-    //let x = add_five(5);
-    asrt();
-}
+fn main() { }
 
-// WP=      x+5 = x+5 & x < MAX-5
-#[condition(pre="(x: i32 < 2147483642:i32) && (x: i32 > -2147483653:i32)", post="return: i32 == (x: i32 +5:i32)")]
+// Should be valid
+#[condition(pre="(x: i32 <= i32::MAX - 4:i32) && (x: i32 >= i32::MIN + 5:i32)", post="return: i32 == (x: i32 +5:i32)")]
 fn add_five(x: i32) -> i32 {
     x+5
-    //assert!(1 > 0)
 }
 
-#[scondition(pre="true", post="true")]
+// Should be valid
+#[condition(pre="true", post="true")]
 fn asrt() {
     assert!(1 > 0)
 }
