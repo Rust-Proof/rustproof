2da75b441e722f870e6957e16493910bb3abd6d9
Matthew Slocum <arco000@gmail.com>
2016-08-11 01:32:20 -0700

    changed output message function signature to be more rustlike

--- src/smt_output/mod.rs
+++ src/smt_output/mod.rs
@@ -50,12 +50,12 @@ pub fn gen_smtlib (vc: &Expression, name: String) {
     match res {
         Ok(..) => {
             match check {
-                SMTRes::Sat(..) => { println!("\nFunction {}\tVerification Condition is not valid.\n{:?}", name, check); },
-                SMTRes::Unsat(..) => { println!("\nFunction {}\tVerification Condition is valid.\n{:?}", name, check); },
+                SMTRes::Sat(..) => { println!("\nfn {}(..)\tVerification Condition is not valid.\n{:?}", name, check); },
+                SMTRes::Unsat(..) => { println!("\nfn {}(..)\tVerification Condition is valid.\n{:?}", name, check); },
                 _ => { unimplemented!() }
             }
         },
-        Err(..) => { println!("\nFunction {}\tError in Verification Condition Generation.\n{:?}", name, check); },
+        Err(..) => { println!("\nfn {}(..)\tError in Verification Condition Generation.\n{:?}", name, check); },
     }
 
     /*
