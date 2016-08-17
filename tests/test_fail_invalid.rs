7b9cfd8afe806a109492ee737e01616b3c5653cb
Michael Salter <salterm@pdx.edu>
2016-08-16 23:28:15 -0700

    Changed comments in test_fail_valid/invalid.

--- tests/test_fail_invalid.rs
+++ tests/test_fail_invalid.rs
@@ -2,11 +2,11 @@
 #![plugin(rustproof)]
 #![allow(dead_code)]
 #![allow(unused_attributes)]
-fn main() {}
+fn main() { }
 
 // Used to test the tester
 
-// Should be valid but function name specifies valid
+// Condition is valid, and function name indicates "invalid", so should be detected as a mismatch
 #[condition(pre="x: i32 <= i32::MAX - 5:i32", post="return: i32 == (x: i32 + 5:i32)")]
 fn invalid_add_five_i32(x: i32) -> i32 {
     x+5
